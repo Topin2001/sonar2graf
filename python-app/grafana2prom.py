f55d6674ae5cbb98c97e4b8d1339833528c160b7
Thomas <thomas.galpin@cnes.fr>
2024-04-09 09:25:21 +0000

    Added persistance with docker volumes + update dashboard + add filter to projects

--- python-app/grafana2prom.py
+++ python-app/grafana2prom.py
@@ -1,10 +1,13 @@
 #!/bin/python3
 
 import requests
-from prometheus_client import start_http_server, Gauge
 import time
 import os
+import urllib3
+
+from math import ceil
 from dotenv import load_dotenv
+from prometheus_client import start_http_server, Gauge
 
 class SonarQubeAPI:
     def __init__(self, url, token):
@@ -12,13 +15,18 @@ class SonarQubeAPI:
         self.token = token
         self.auth = (self.token, '')
 
-    def get_project_info(self, project_key):
-        response = requests.get(f'{self.url}/api/project_analyses/search?project={project_key}', auth=self.auth, verify=False)
+    def get_project_from_page(self, page):
+        response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK&p={page}', auth=self.auth, verify=False)
         return response.json()
-    
+
     def get_project_list(self):
         response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK', auth=self.auth, verify=False)
-        return response.json()
+        page_number = ceil(response.json()['paging']['total']/response.json()['paging']['pageSize'])
+        response = response.json()['components']
+        if page_number > 1 :
+            for i in range(2, page_number+1):
+                response.extend(self.get_project_from_page(i)['components'])
+        return response
     
     def get_project_issues(self, project_key):
         response = requests.get(f'{self.url}/api/issues/search?componentKeys={project_key}', auth=self.auth, verify=False)
@@ -42,14 +50,17 @@ class SonarQubeAPI:
             print(f"Error: {err}")
             return False
 
+urllib3.disable_warnings()
+#Load values from .env file
 load_dotenv()
 
 sonar_key = os.getenv('SONAR_KEY')
 sonar_url = os.getenv('SONAR_URL')
 http_port = int(os.getenv('HTTP_PORT'))
+max_projects = int(os.getenv('MAX_PROJECTS'))
 
+#Create Sonar and connect to Sonarqube
 sonar = SonarQubeAPI(sonar_url, sonar_key)
-
 
 # Check connection to SonarQube API
 while not sonar.check_connection():
@@ -66,17 +77,11 @@ sonarqube_project_errors_total = Gauge('sonarqube_project_errors_total', 'SonarQ
 sonarqube_project_errors_blocker = Gauge('sonarqube_project_errors_blocker', 'SonarQube project errors blocker', ['project_key', 'project_name'])
 
 def sonar_error_gauge(project_list):
-    for project in project_list['components']:
+    for project in project_list:
         project_key = project['key']
         project_name = project['name']
         project_issues = sonar.get_project_issues(project_key)
         sonarqube_project_errors_total.labels(project_key=project_key, project_name=project_name).set(project_issues['total'])
-
-def sonar_blocker_gauge(project_list):
-    for project in project_list['components']:
-        project_key = project['key']
-        project_name = project['name']
-        project_issues = sonar.get_project_issues(project_key)
         blocker_count = sum(1 for issue in project_issues['issues'] if issue['severity'] == 'BLOCKER')
         sonarqube_project_errors_blocker.labels(project_key=project_key, project_name=project_name).set(blocker_count)
 
@@ -85,8 +90,10 @@ def sonar_blocker_gauge(project_list):
 start_http_server(http_port)
 while True:
     project_list = sonar.get_project_list()
+    project_list = [i for i in project_list if "ISIS_" in i['key']]
+    if len(project_list) > max_projects:
+        print('Too many projects, risk of overloading SonarQube ! \n By default max 100 projects, can be update in .env file.')
+        exit(1)
     sonar_error_gauge(project_list)
-    sonar_blocker_gauge(project_list)
-    
-    time.sleep(15)
+    time.sleep(3600)
     
--- python-app/start.sh
+++ python-app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PIP_INDEX_URL=https://<add_repo_manager>/artifactory/api/pypi/pypi/simple && \
-pip install --upgrade pip --trusted-host artifactory.cnes.fr && \
+pip install --upgrade pip --trusted-host <repo_manager_host> && \
 pip install --no-cache-dir -r requirements.txt --trusted-host artifactory.cnes.fr && \
 echo 'install finish' && \
 python3 grafana2prom.py
