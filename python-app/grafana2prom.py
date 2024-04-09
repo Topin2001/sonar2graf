874609bea6fded9408f2793fed48eed550c82cbd
Thomas <thomas.galpin@cnes.fr>
2024-04-09 14:23:48 +0000

    fix ssl with crt file and add logging option

--- python-app/grafana2prom.py
+++ python-app/grafana2prom.py
@@ -3,7 +3,8 @@
 import requests
 import time
 import os
-import urllib3
+import logging
+import argparse
 
 from math import ceil
 from dotenv import load_dotenv
@@ -16,11 +17,11 @@ class SonarQubeAPI:
         self.auth = (self.token, '')
 
     def get_project_from_page(self, page):
-        response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK&p={page}', auth=self.auth, verify=False)
+        response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK&p={page}', auth=self.auth, verify=cert_file)
         return response.json()
 
     def get_project_list(self):
-        response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK', auth=self.auth, verify=False)
+        response = requests.get(f'{self.url}/api/components/search?ps=500&qualifiers=TRK', auth=self.auth, verify=cert_file)
         page_number = ceil(response.json()['paging']['total']/response.json()['paging']['pageSize'])
         response = response.json()['components']
         if page_number > 1 :
@@ -29,53 +30,26 @@ class SonarQubeAPI:
         return response
     
     def get_project_issues(self, project_key):
-        response = requests.get(f'{self.url}/api/issues/search?componentKeys={project_key}', auth=self.auth, verify=False)
+        response = requests.get(f'{self.url}/api/issues/search?componentKeys={project_key}', auth=self.auth, verify=cert_file)
         return response.json()
     
     def check_connection(self):
        try:
-           response = requests.get(f'{self.url}/api/system/status', auth=self.auth, verify=False)
+           response = requests.get(f'{self.url}/api/system/status', auth=self.auth, verify=cert_file)
            response.raise_for_status()
            return True
        except requests.exceptions.RequestException as err:
-           print(f"Error: {err}")
+           logging.debug(f"Error: {err}")
            return False
        
     def check_authentication(self):
         try:
-            response = requests.get(f'{self.url}/api/user_tokens/search', auth=self.auth, verify=False)
+            response = requests.get(f'{self.url}/api/user_tokens/search', auth=self.auth, verify=cert_file)
             response.raise_for_status()
             return True
         except requests.exceptions.RequestException as err:
-            print(f"Error: {err}")
+            logging.debug(f"Error: {err}")
             return False
-
-urllib3.disable_warnings()
-#Load values from .env file
-load_dotenv()
-
-sonar_key = os.getenv('SONAR_KEY')
-sonar_url = os.getenv('SONAR_URL')
-filter = os.getenv('FILTER')
-http_port = int(os.getenv('HTTP_PORT'))
-max_projects = int(os.getenv('MAX_PROJECTS'))
-
-#Create Sonar and connect to Sonarqube
-sonar = SonarQubeAPI(sonar_url, sonar_key)
-
-# Check connection to SonarQube API
-while not sonar.check_connection():
-    print("Unable to connect to SonarQube API. Retrying in 5 seconds...")
-    time.sleep(5)
-
-# Check authentication to SonarQube API
-while not sonar.check_authentication():
-    print("Unable to authenticate to SonarQube API. Please check your token.")
-    exit(1)
-
-
-sonarqube_project_errors_total = Gauge('sonarqube_project_errors_total', 'SonarQube project errors', ['project_key', 'project_name'])
-sonarqube_project_errors_blocker = Gauge('sonarqube_project_errors_blocker', 'SonarQube project errors blocker', ['project_key', 'project_name'])
 
 def sonar_error_gauge(project_list):
     for project in project_list:
@@ -86,15 +60,76 @@ def sonar_error_gauge(project_list):
         blocker_count = sum(1 for issue in project_issues['issues'] if issue['severity'] == 'BLOCKER')
         sonarqube_project_errors_blocker.labels(project_key=project_key, project_name=project_name).set(blocker_count)
 
+#Retreive arguments given while starting the script
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='Prometheus collector for a sonarqube server')
+parser.add_argument(
+    "--verbose",
+    "-v",
+    required=False,
+    help="increase output verbosity",
+    action="store_true")
+args = parser.parse_args()
+
+#Set logging level
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.captureWarnings(True)
+else:
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    
+
+#Load values from .env file
+load_dotenv()
+sonar_key = os.getenv('SONAR_KEY')
+sonar_url = os.getenv('SONAR_URL')
+filter = os.getenv('FILTER')
+cert_file = os.getenv('CERT_FILE')
+http_port = int(os.getenv('HTTP_PORT'))
+max_projects = int(os.getenv('MAX_PROJECTS'))
+
+#Create Sonar and connect to Sonarqube
+sonar = SonarQubeAPI(sonar_url, sonar_key)
+
+# Check connection to SonarQube API
+logging.debug('Check connection to SonarQube API.')
+error_connect_compt = 1
+while not sonar.check_connection():
+    if error_connect_compt > 4 :
+        logging.critical("Unable to connect to SonarQube API. Please check your connection or configuration.")
+        exit(1)
+    logging.error(f'Unable to connect to SonarQube API on try {error_connect_compt}. Retrying in 5 seconds...')
+    error_connect_compt +=1
+    time.sleep(5)
+logging.debug('Connection to SonarQube API established.')
+
+
+# Check authentication to SonarQube API
+logging.debug('Check authentification to SonarQube API')
+while not sonar.check_authentication():
+    logging.critical("Unable to authenticate to SonarQube API. Please check your token.")
+    exit(1)
+logging.debug('Authentification to SonarQube API done.')
+
+
+#Create the Prometheus gauge
+sonarqube_project_errors_total = Gauge('sonarqube_project_errors_total', 'SonarQube project errors', ['project_key', 'project_name'])
+sonarqube_project_errors_blocker = Gauge('sonarqube_project_errors_blocker', 'SonarQube project errors blocker', ['project_key', 'project_name'])
 
 # Start up the server to expose the metrics.
+logging.debug(f'Start http server on port {http_port}')
 start_http_server(http_port)
 while True:
+    logging.debug('Genrating project list')
     project_list = sonar.get_project_list()
+    logging.debug('Filtering project list')
     project_list = [i for i in project_list if filter in i['key']]
     if len(project_list) > max_projects:
-        print('Too many projects, risk of overloading SonarQube !\nBy default max 100 projects, can be update in .env file.')
+        logging.critical(f'Too many projects ({len(project_list)}), risk of overloading SonarQube !\nBy default max 100 projects, can be update in .env file.')
         exit(1)
     sonar_error_gauge(project_list)
+    logging.debug('All done, waiting 1h before next scrape.')
     time.sleep(3600)
     
